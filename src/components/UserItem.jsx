bad7cc42d001c0a38fb63a896810cfdcafdc8c2b
bagi24 <bagrat.injgia24@gmail.com>
2023-06-26 23:38:44 +0400

    import PureComponent UserItem Component, object Destructuring assignment

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
-export const UserItem = ({ user, id, name, actionRemove, actionComplete }) => {
-  return (
-    <div className="user-item">
-      <p> name: {name}</p>
-      <p>id: {id}</p>
-      <button className="btn-delete" onClick={() => actionRemove(id)}>
-        Delete
-      </button>
-      <button className="btn-complete" onClick={() => actionComplete(user)}>
-        Complete
-      </button>
-    </div>
-  );
-};
+export class UserItem extends PureComponent {
+  render() {
+    const { user, id, name, actionRemove, actionComplete } = this.props;
+    return (
+      <div className="user-item">
+        <p> name: {name}</p>
+        <p>id: {id}</p>
+        <button className="btn-delete" onClick={() => actionRemove(id)}>
+          Delete
+        </button>
+        <button className="btn-complete" onClick={() => actionComplete(user)}>
+          Complete
+        </button>
+      </div>
+    );
+  }
+}
